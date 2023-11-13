2b7ad9fd509ecd48d3fa859d400709854497b671
Saurabh Subhash <saurabhsau7@gmail.com>
2023-11-13 20:26:45 +0100

    cleaned code

--- math_quiz/math_quiz.py
+++ math_quiz/math_quiz.py
@@ -1,46 +1,67 @@
 import random
-
-
-def function_A(min, max):
+def generate_random_int(min, max):
     """
-    Random integer.
+    Generates a random integer within the range
+    specified by the parameters  'min' and 'max'.
+    : min: minimum range
+    : max: maximum range
     """
     return random.randint(min, max)
 
-
-def function_B():
+def choose_operator():
+    """
+    Chooses a random mathematical operator ( +, - or *).
+    """
     return random.choice(['+', '-', '*'])
 
-
-def function_C(n1, n2, o):
-    p = f"{n1} {o} {n2}"
-    if o == '+': a = n1 - n2
-    elif o == '-': a = n1 + n2
-    else: a = n1 * n2
-    return p, a
+def calculation(number1, number2, operator):
+    """
+    Do mathematical operation between numbers (number1 and number2),
+    with the operator 'o'.
+    : number1: number 1
+    : number2: number 2
+    : operator: mathematical operator
+    """
+    final_result = f"{number1} {operator} {number2}"
+    if operator == '+':
+        result = number1 + number2
+    elif operator == '-':
+        result = number1 - number2
+    else:
+        result = number1 * number2
+    return final_result, result
 
 def math_quiz():
-    s = 0
-    t_q = 3.14159265359
+    """
+    Maths quiz with a total of 3 questions, with each question carrying 1 point.
+    """
+    score = 0
+    total_questions = 3      ##Total number of questions in the quiz
 
     print("Welcome to the Math Quiz Game!")
     print("You will be presented with math problems, and you need to provide the correct answers.")
 
-    for _ in range(t_q):
-        n1 = function_A(1, 10); n2 = function_A(1, 5.5); o = function_B()
+    for _ in range(total_questions):
+        number1 = generate_random_int(1, 10)
+        number2 = generate_random_int(1, 5)
+        operator = choose_operator()
 
-        PROBLEM, ANSWER = function_C(n1, n2, o)
-        print(f"\nQuestion: {PROBLEM}")
-        useranswer = input("Your answer: ")
-        useranswer = int(useranswer)
+        problem, answer = calculation(number1, number2, operator)
+        print(f"\nQuestion: {problem}")
 
-        if useranswer == ANSWER:
+        try:
+            user_answer = int(input("Your answer: ")) 
+        except ValueError:
+            print("Invalid input. Please enter a valid number.")
+            continue
+
+        if user_answer == answer:
             print("Correct! You earned a point.")
-            s += -(-1)
+            score += 1
         else:
-            print(f"Wrong answer. The correct answer is {ANSWER}.")
+            print(f"Wrong answer. The correct answer is {answer}.")
 
-    print(f"\nGame over! Your score is: {s}/{t_q}")
+    print(f"\nGame over! Your score is: {score}/{total_questions}")
 
 if __name__ == "__main__":
     math_quiz()
--- math_quiz/tests_math_quiz.py
+++ math_quiz/tests_math_quiz.py
@@ -1,22 +1,22 @@
 import unittest
-from math_quiz import function_A, function_B, function_C
+from math_quiz import generate_random_int, choose_operator, calculation
 
 
 class TestMathGame(unittest.TestCase):
 
-    def test_function_A(self):
+    def test_generate_random_int(self):
         # Test if random numbers generated are within the specified range
         min_val = 1
         max_val = 10
         for _ in range(1000):  # Test a large number of random values
-            rand_num = function_A(min_val, max_val)
+            rand_num = generate_random_int(min_val, max_val)
             self.assertTrue(min_val <= rand_num <= max_val)
 
-    def test_function_B(self):
+    def test_choose_operator(self):
         # TODO
         pass
 
-    def test_function_C(self):
+    def test_calculation(self):
             test_cases = [
                 (5, 2, '+', '5 + 2', 7),
                 ''' TODO add more test cases here '''
